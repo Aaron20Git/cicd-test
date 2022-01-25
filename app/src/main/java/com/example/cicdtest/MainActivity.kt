59a0b408aa4f0504f08e2902a1fe9e4bfced3493
Aaron <findtherule@gmail.com>
2022-01-25 17:58:04 +0800

    first created

---
+++ app/src/main/java/com/example/cicdtest/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.cicdtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tvTitle = findViewById<TextView>(R.id.tv_title)
+
+        tvTitle.text = "Hello World. This is CICD Test"
+    }
+}
