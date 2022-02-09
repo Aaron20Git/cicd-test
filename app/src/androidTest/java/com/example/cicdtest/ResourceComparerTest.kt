e2df25fcc20ff7a9360ece4b09dd483667140b4a
Aaron <findtherule@gmail.com>
2022-02-09 14:35:22 +0800

    1.add Android Instrumentation Tests 2.update master.yml- update master.yml

---
+++ app/src/androidTest/java/com/example/cicdtest/ResourceComparerTest.kt
@@ -0,0 +1,37 @@
+package com.example.cicdtest
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class ResourceComparerTest {
+
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setup() {
+        resourceComparer = ResourceComparer()
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_returnsTrue() {
+        val result = resourceComparer.isEqual(
+            ApplicationProvider.getApplicationContext<Context>(),
+            R.string.app_name,
+            "CICDTest"
+        )
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferentAsGivenString_returnsFalse() {
+        val result = resourceComparer.isEqual(
+            ApplicationProvider.getApplicationContext<Context>(),
+            R.string.app_name,
+            "Hello"
+        )
+        assertThat(result).isFalse()
+    }
+}
---
+++ app/src/main/java/com/example/cicdtest/ResourceComparer.kt
@@ -0,0 +1,10 @@
+package com.example.cicdtest
+
+import android.content.Context
+
+class ResourceComparer {
+
+    fun isEqual(ctx: Context, resId: Int, string: String): Boolean {
+        return ctx.getString(resId) == string
+    }
+}
