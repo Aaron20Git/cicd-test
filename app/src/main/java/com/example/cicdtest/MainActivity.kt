bac08b0e02a5024a66119266e58f4aea24353510
Aaron <findtherule@gmail.com>
2022-03-07 17:06:11 +0800

    update code- 1.add basic UI 2.add basic logic

--- app/src/main/java/com/example/cicdtest/MainActivity.kt
+++ app/src/main/java/com/example/cicdtest/MainActivity.kt
@@ -2,22 +2,57 @@ package com.example.cicdtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tvTitle:TextView
+    lateinit var etName:EditText
+    lateinit var etPassword:EditText
+    lateinit var etPasswordCheck:EditText
+    lateinit var btnCheck:Button
+    lateinit var etAPPName:EditText
+    lateinit var btnAPPCheck:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tvTitle = findViewById<TextView>(R.id.tv_title)
+        getView()
+        setListener()
+    }
 
-        tvTitle.text = "Hello World. This is CICD Test"
+    private fun getView() {
+        tvTitle = findViewById<TextView>(R.id.tv_title)
+        etName = findViewById<EditText>(R.id.et_name)
+        etPassword = findViewById<EditText>(R.id.et_password)
+        etPasswordCheck = findViewById<EditText>(R.id.et_password_doublecheck)
+        btnCheck = findViewById<Button>(R.id.btn_check)
+        etAPPName = findViewById<EditText>(R.id.et_app_name)
+        btnAPPCheck = findViewById<Button>(R.id.btn_app_check)
+    }
 
-        // 第一次更改
-        tvTitle.text = "Hello World. This is CICD Test, First Change"
-        tvTitle.text = "Hello World. This is CICD Test, 2 Change"
-        tvTitle.text = "Hello World. This is CICD Test, 3 Change"
-        tvTitle.text = "Hello World. This is CICD Test, 4 Change"
-        tvTitle.text = "Hello World. This is CICD Test, 5 Change"
+    private fun setListener() {
+        btnCheck.setOnClickListener {
+            FunctionUtils.validateRegistrationInput(etName.text.toString(),
+            etPassword.text.toString(),
+            etPasswordCheck.text.toString()).run {
+                if (this)
+                    Toast.makeText(this@MainActivity,"成功",Toast.LENGTH_SHORT).show()
+                else
+                    Toast.makeText(this@MainActivity,"錯誤",Toast.LENGTH_SHORT).show()
+            }
+        }
+        btnAPPCheck.setOnClickListener {
+            ResourceComparer().isEqual(this,R.string.app_name,etAPPName.text.toString()).run {
+                if (this)
+                    Toast.makeText(this@MainActivity,"名稱正確",Toast.LENGTH_SHORT).show()
+                else
+                    Toast.makeText(this@MainActivity,"名稱錯誤",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
